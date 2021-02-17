b82617b046d9bfe1a46ef4a858d865e2f1b850da
syabii <57247020+syabii@users.noreply.github.com>
2021-02-17 17:39:35 +0800

    Update youth.js

--- Task/youth.js
+++ Task/youth.js
@@ -64,7 +64,7 @@ https:\/\/ios\.baertt\.com\/v5\/user\/app_stay\.json url script-request-body you
 [MITM]
 hostname = *.youth.cn, ios.baertt.com 
 ~~~~~~~~~~~~~~~~
-
+*/
 let s = 1000 //各数据接口延迟
 const $ = new Env("中青看点")
 let notifyInterval = $.getdata("notifytimes")||50 //通知间隔，默认抽奖每50次通知一次，如需关闭全部通知请设为0
